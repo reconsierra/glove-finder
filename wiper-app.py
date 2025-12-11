ffa8bb6c395ed1bf26053eec254eddb6306abc13
Rec0n <offers@extensive.com.au>
2025-12-12 09:39:56 +1100

    Update wiper-app.py

--- wiper-app.py
+++ wiper-app.py
@@ -12,7 +12,7 @@ st.caption("Find the right glove by cut level/category, colour and safety attrib
 
 # =========================================================
 # 🔧 DISPLAY ORDER — adjust here (app creator only)
-# Valid labels: "Colour (predominant colour only)", "Cut Category - (new style letter type)", "Cut rating (old style number type)",
+# Valid labels: "Colour", "Cut Category", "Cut rating",
 #               "Food Safe?", "Chemical rated?", "Heat rated?"
 ORDER_LEFT:  List[str] = ["Colour", "Cut Category", "Cut rating"]
 ORDER_RIGHT: List[str] = ["Food Safe?", "Chemical rated?", "Heat rated?"]
